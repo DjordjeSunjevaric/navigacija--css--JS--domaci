a8b393804ebfd0411468db7cdc3ce5f83fb2ebff
DjordjeSunjevaric <djordjesunjevaric@yahoo.com>
2013-07-13 03:08:05 +0200

    ubacen stop()

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
 	var $link = $('a');
 	$link.hover(function(){
 
-		$(this).animate({
+		$(this).stop().animate({
 			width:'100px',
 		})
 
 
 	},function(){
 
-		$(this).animate({
-			width:'55px'
+		$(this).stop().animate({
+			width:'55px',
 		})
 
 	})
